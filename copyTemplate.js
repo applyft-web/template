e0b5239b5c53c1781cf808495562bdd7066673ee
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-05-27 02:05:57 +0100

    script update

--- copyTemplate.js
+++ copyTemplate.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const templateDir = path.join(__dirname, 'template');
+const templateDir = path.join(__dirname, 'resources');
 const targetDir = process.cwd();
 
 fs.copy(templateDir, targetDir, err => {
