6b79c52c3237d34fd3f525582d62c92382c82bc8
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-05-27 16:42:28 +0100

    script update

--- copyTemplate.js
+++ copyTemplate.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs-extra');
 const path = require('path');
+const { exec } = require('child_process');
 
 const templateDir = path.join(__dirname, 'resources');
 const destinationDir = process.cwd();
@@ -10,9 +11,23 @@ async function copyTemplate() {
   try {
     await fs.copy(templateDir, destinationDir);
     console.log('Template copied successfully!');
+    installDependencies();
   } catch (err) {
     console.error('Error copying template:', err);
   }
+}
+
+function installDependencies() {
+  console.log('Installing dependencies...');
+  exec('npm install', (err, stdout, stderr) => {
+    if (err) {
+      console.error('Error installing dependencies:', err);
+      return;
+    }
+    console.log(stdout);
+    console.error(stderr);
+    console.log('Dependencies installed successfully!');
+  });
 }
 
 copyTemplate();
